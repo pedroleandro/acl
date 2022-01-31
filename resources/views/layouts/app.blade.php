82a2b0142e8d38b623b1be7a0d01191aab7a8a32
pedroleandro <pedro.leandrog@gmail.com>
2022-01-31 12:10:36 -0300

    CRUD Perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,8 @@
 
                                 <a class="dropdown-item" href="{{ route('user.index') }}">Gestão de Usuários</a>
 
+                                <a class="dropdown-item" href="{{ route('role.index') }}">Gestão de Perfis</a>
+
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
